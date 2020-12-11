1bea5212cf3e3d6a36072079662fd4c94c5fb5bb
Jason Nordheim <jason.nordheim@gmail.com>
2020-12-10 21:13:40 -0700

    Created first Database context and defined a table named `Values` to hold `Value` objects

---
+++ projects/reactivities/Persistence/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DataContext : DbContext 
+    {
+        /* must pass `options` to base for migrations to work   */
+        public DataContext(DbContextOptions options): base(options)
+        {
+            
+        }
+
+        /* this will be used to generate the table name  */
+        public DbSet<Value> Values { get; set;}
+    }
+}
